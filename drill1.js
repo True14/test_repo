08b911b0b4ab8644285af74659a6c3d620f4030c
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-05-08 12:46:51 -0400

    add year of birth

--- drill1.js
+++ drill1.js
@@ -1,6 +1,7 @@
 function whoAmI(name , age) {
+  const yearOfBirth = 2016 - age;
   console.log(`Hi my name is ${name} and I\'m ${age} years old`);
+  console.log(`I was born in ${yearOfBirth}`);
 }
 
 whoAmI('william','23');
-

3fda92df387aff830ecf52294ad6da548e10cbee
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-05-08 14:35:28 -0400

    exercise 6 and 7

--- drill1.js
+++ drill1.js
@@ -1,11 +1,24 @@
 function yearOfBirth(age){
-	const yearOfBirth = 2016 - age;
+  if(age < 0){
+    throw new Error("Age can not be negative");
+  }
+  return 2016 - age;
 }
 
 function whoAmI(name , age) {
-  const yob = yearOfBirth(age);
-  console.log(`Hi my name is ${name} and I\'m ${age} years old`);
-  console.log(`I was born in ${yearOfBirth}`);
+  if(!name || !age){
+    console.log("Arguments not valid");
+  }
+  else {
+      try {
+        const yob = yearOfBirth(age);
+        console.log(`Hi my name is ${name} and I\'m ${age} years old`);
+        console.log(`I was born in ${yob}`);
+      }
+      catch(e) {
+      console.log('Something is wrong: ' + e.message);
+    }
+  }
 }
 
-whoAmI('william','23');
+whoAmI();
